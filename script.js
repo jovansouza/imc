1a9fffa2d6a3eef69dd5dcdef3a78b0a1fac5325
jovansouza <jovansouza14@gmail.com>
2023-04-19 10:33:02 -0300

    alert

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ const peso = document.getElementById('peso').value;
 const valorimc = peso/(altura*altura) ;
 const text =''
 if(valorimc <18.5){
-   
+   alert ('tá magrin')
 }else if(valorimc <24.9){
- 
+    alert ('tá um pitel')
 }else if(valorimc <29.9){
- 
+    alert ('tá gordin')
 }
 
 document.getElementById('text_area').innerText= valorimc
