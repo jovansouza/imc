54d1b8419553f6618bdb5a8990e3c075d299f169
jovansouza <jovansouza14@gmail.com>
2023-04-20 10:37:58 -0300

    atualizei

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ const peso = document.getElementById('peso').value;
 
 
 
-const valorimc = (peso/(altura*altura )).toFixed(1) ;
+const valorimc = ( peso/(altura*altura )).toFixed(1) ;
 
 
 if(valorimc <18.5){
    alert (`seu IMC é ${valorimc}, então voçê tá magrin!`);
 }else if(valorimc <24.9){
     alert (`seu IMC é ${valorimc}, então voçê tá um pitel!`)
-}else if(valorimc <29.9){
+}else if(valorimc > 30){
     alert (`seu IMC é ${valorimc}, então voçê tá gordin`)
 }
 
-document.getElementById('text_area').innerText= valorimc
+document.getElementById('text_area').innerText= `seu imc é = ${valorimc} `
 
 }
 
